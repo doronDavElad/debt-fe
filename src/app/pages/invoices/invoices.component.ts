5a5898d0c037e0f80a0a3dc136caf8fdb323e1eb
doronDavElad <dorond@elad.co.il>
2025-01-19 15:55:17 +0200

    select input

--- src/app/pages/invoices/invoices.component.ts
+++ src/app/pages/invoices/invoices.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { SubHeaderComponent } from '../../components/sub-header/sub-header.component';
 import { GenericTableComponent } from '../../components/generic-table/generic-table.component';
 import { ItableValues } from '../../components/generic-table/table-interface';
 import { invoicesTable } from '../../mockData/bills';
+import { Itabs_sub_header } from '../../components/sub-header/subHeader_mockData';
+import { IgenericInput } from '../../components/generic-input/genericInput.interface';
+import { GenericInputComponent } from "../../components/generic-input/generic-input.component";
 
 @Component({
   selector: 'app-invoices',
   standalone: true,
-  imports: [SubHeaderComponent,GenericTableComponent],
+  imports: [SubHeaderComponent, GenericTableComponent, GenericInputComponent],
   templateUrl: './invoices.component.html',
-  styleUrl: './invoices.component.scss'
+  styleUrl: './invoices.component.scss',
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class InvoicesComponent {
 
   tableData: any[] = invoicesTable;
+
+  
   tableInvoiceMockData:ItableValues[]=[
     {
       controlName: 'customerId',
@@ -71,6 +78,45 @@ export class InvoicesComponent {
       sort:true
     },
   ]
+
+  handleInputOutput(emittedData:{value:string,index:number}){
+    console.log(emittedData);
+    
+
+  }
+
+  taskeInputs:IgenericInput[]=[
+    {
+      type:'text',
+      placeHolder:'חיפוש לפי מספר חשבונית או שם לקוח',
+      options:[],
+      icon:'../../../assets/images/search.svg',
+      value:''
+    },
+    {
+      type:'date',
+      placeHolder:'תאריך ערך',
+      options:[],
+      icon:'',
+      value:''
+    },
+    {
+      type:'select',
+      placeHolder:'סינון:',
+      options:["הכל","לא נשלחה","נשלחה","נדחתה","אושרה לתשלום",'ללא תגובה','אושרה' ,'לתשלום'],
+      icon:'../../../assets/images/chevron_down.svg',
+      value:''
+    }
+  ]
+  dataToTable: Itabs_sub_header = {
+    value: '',
+    title: '',
+    img: '',
+  }; 
+  onTabsDataReceived(tab: Itabs_sub_header) {
+    this.dataToTable = tab; 
+  }
+
 
 
 }
